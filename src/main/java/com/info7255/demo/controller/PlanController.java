a74d892e65d217f64c837f99508e1dcee7ad77cf
Priyam Suthar <suthar.p@northeastern.edu>
2021-02-19 12:05:23 -0500

    clean code

--- src/main/java/com/info7255/demo/controller/PlanController.java
+++ src/main/java/com/info7255/demo/controller/PlanController.java
@@ -19,7 +19,6 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -33,7 +32,7 @@ public class PlanController {
     }
 
     @PostMapping(value = "/plan", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> createPlan( @RequestBody String planObject ) throws URISyntaxException {
+    public ResponseEntity<?> createPlan( @RequestBody String planObject ) {
         JSONObject plan = new JSONObject(planObject);
         JSONObject schemaJSON = new JSONObject(new JSONTokener(PlanController.class.getResourceAsStream("/plan-schema.json")));
         Schema schema = SchemaLoader.load(schemaJSON);
@@ -55,8 +54,7 @@ public class PlanController {
 
     @GetMapping(value = "/plan/{objectId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getPlan( @PathVariable String objectId, @RequestHeader HttpHeaders headers ) {
-        if ( !planService.isKeyPresent(objectId) )
-            throw new ResourceNotFoundException("Object with the given objectID not found!");
+        if ( !planService.isKeyPresent(objectId) ) throw new ResourceNotFoundException("Object with the given objectID not found!");
 
         JSONObject object = planService.getPlan(objectId);
         String eTag = eTagService.getETag(object);
@@ -78,7 +76,7 @@ public class PlanController {
     }
 
     @DeleteMapping("/plan/{objectId}")
-    public ResponseEntity<String> deletePlan( @PathVariable String objectId ) {
+    public ResponseEntity<?> deletePlan( @PathVariable String objectId ) {
         if ( !planService.isKeyPresent(objectId) ) throw new ResourceNotFoundException("Plan not found!");
         planService.deletePlan(objectId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
